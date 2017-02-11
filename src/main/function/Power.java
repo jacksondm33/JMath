f55ce5cf65a9339d333812a9f84466a470b8527d
Jackson McClintock <jacksondm33@gmail.com>
2017-02-11 17:15:53 -0500

    Added Java Docs

--- src/main/function/Power.java
+++ src/main/function/Power.java
@@ -9,16 +9,32 @@ public class Power implements Function {
     private Constant n;
     private boolean reciprocal;
 
+    /**
+     * x ^ n
+     * @param constants  Constants
+     * n = constants[0]
+     * @param reciprocal  If the function is reciprocated
+     */
     public Power(Constant[] constants, boolean reciprocal){
         n = constants[0];
         this.reciprocal = reciprocal;
     }
 
+    /**
+     * Initializes function with constants
+     * @param constants Constants
+     * n = constants[0]
+     */
     @Override
     public void init(Constant[] constants) {
         n = constants[0];
     }
 
+    /**
+     * Solves the function with given variable values
+     * @param variables  Variable values
+     * @return Solution
+     */
     @Override
     public double solve(Variable[] variables) {
         if(reciprocal()){
@@ -28,11 +44,19 @@ public class Power implements Function {
         }
     }
 
+    /**
+     * Returns whether the function is reciprocated
+     * @return reciprocal
+     */
     @Override
     public boolean reciprocal() {
         return reciprocal;
     }
 
+    /**
+     * Solves for the derivative of the function
+     * @return Derivative
+     */
     @Override
     public Expression derivative() {
         return new SimpleExpression(new Term[]{
@@ -44,8 +68,12 @@ public class Power implements Function {
         });
     }
 
+    /**
+     * Solves for the antiderivative of the function
+     * @return Antiderivative
+     */
     @Override
-    public Expression antiDerivative() {
+    public Expression antiderivative() {
         return new SimpleExpression(new Term[]{
                 new SimpleTerm(new SimpleConstant(1 / (n.value() + 1)), new Function[]{
                         new Power(new Constant[]{
@@ -55,6 +83,10 @@ public class Power implements Function {
         });
     }
 
+    /**
+     * Returns a String representation of the function
+     * @return String representation
+     */
     @Override
     public String stringRep() {
         return "x^" + n.value();
